d680227de42cb1dfbd5ade5fe5218fcb4032d1aa
Unknown <ro.wemmers@gmail.com>
2018-02-20 13:26:28 +0100

    Changed overview to Layout
    Includes layouts.app

--- resources/views/overview.blade.php
+++ resources/views/overview.blade.php
@@ -1,28 +1,4 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
-    <title>{{config('app.name' , 'FilmOverview')}}</title>
-    <title>Document</title>
-    <style>
-        .Movie {
-            width: 650px;
-            margin: 2px auto;
-            padding: 20px;
-            background: #999999;
-        }
-        .important {
-            background: red;
-            padding: 20px;
-        }
-    </style>
-</head>
-<body>
-    <h1 class="important">STYLE IS NIET BLIJVEND EN OOK NIET BELANGRIJK</h1>
+@include('layouts.app')
 
     {{--This part can be used for the LaraFilm Project--}}
     @foreach($movies as  $movie)
@@ -36,5 +12,3 @@
     @endforeach
     {{--Ends here!--}}
 
-</body>
-</html>
